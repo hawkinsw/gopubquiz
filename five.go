437d08e56db9b21c4db5de4eaeaeb19b751415a1
Will Hawkins <hawkinsw@borlaugic.com>
2018-09-01 18:59:20 -0400

    Add qs five and six.

---
+++ five.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+Taken from https://pliutau.com/go-lang-test/
+The order of execution of defer'd executions.
+Ref:  https://golang.org/ref/spec#Defer_statements
+*/
+import "fmt"
+
+func main() {
+	for i := 0; i < 4; i++ {
+		defer fmt.Print(i)
+	}
+}
---
+++ six.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+Taken from https://pliutau.com/go-lang-test/
+Capturing values of parameters to a defer'd function
+Ref:  https://golang.org/ref/spec#Defer_statements
+*/
+import "fmt"
+
+func main() {
+	j := 4
+	for i := 0; i < 4; i++ {
+		defer func(k int) {
+			fmt.Print(k)
+			j--
+		}(j)
+	}
+}

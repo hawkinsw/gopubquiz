0f6e8b54aee549ea5051b0f075d4d5b866c3af86
Will Hawkins <hawkinsw@borlaugic.com>
2018-09-01 19:50:57 -0400

    Final questions!

---
+++ composite2.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+Taken from https://twitter.com/davecheney/status/1031389514890006528
+Calling method
+Ref: https://golang.org/ref/spec#Method_expressions
+*/
+import "fmt"
+
+type Q struct{}
+
+func (Q) Hola() string { return "Bueno" }
+
+func main() {
+	var q Q
+	fmt.Println(Q.Hola(q))
+}
